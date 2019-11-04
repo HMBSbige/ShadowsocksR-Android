812df1c42736af526e75bc63bb770c6c332624cb
HMBSbige <admin@bige0.com>
2019-11-04 21:32:30 +0800

    Convert NumberPickerPreference
    32/40

---
+++ app/src/main/java/com/github/shadowsocks/preferences/SummaryDialogPreference.kt
@@ -0,0 +1,16 @@
+package com.github.shadowsocks.preferences
+
+import android.content.*
+import android.preference.*
+import android.util.*
+import java.util.*
+
+abstract class SummaryDialogPreference internal constructor(context: Context, attrs: AttributeSet) : DialogPreference(context, attrs)
+{
+	abstract val summaryValue: Any?
+
+	override fun getSummary(): CharSequence
+	{
+		return String.format(Locale.ENGLISH, super.getSummary().toString(), summaryValue)
+	}
+}
